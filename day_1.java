5cd4980cb93842b8ba418f51af9714978ed4f0fd
Radhika <248radhika@gmail.com>
2021-10-17 18:43:04 +0530

    day_1

---
+++ day_1.java
@@ -0,0 +1,20 @@
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        int j=scan.nextInt();
+        double k=scan.nextDouble();
+        String str=scan.nextLine();
+        str=scan.nextLine();
+        System.out.println(j+i);
+        System.out.println(k+d);
+        System.out.println(s+str);
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
